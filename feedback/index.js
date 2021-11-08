161c60b667f0094f5be8d3b1f5065222668f9439
miutamihai <miuta.mihai@gmail.com>
2021-11-08 17:06:16 +0200

    [master] Disable submit if invalid

--- feedback/index.js
+++ feedback/index.js
@@ -2,18 +2,25 @@ const good = ['nice', 'pretty', 'beautiful', 'cool', 'good', 'Good', 'Nice', 'Pr
 
 const bad = ['ugly', 'nasty', 'lame', 'bad', 'Bad', 'Ugly', 'Nasty', 'Lame']
 
+const setSubmitDisabled = value => {
+	document.getElementById('submit').disabled = value
+}
+
 const onSamyOpinionChange = value => {
 	if(good.includes(value)) {
 		document.getElementById('opinionSamy').classList.add('is-valid')
 		document.getElementById('opinionSamyGood').style.display = 'block'
+		setSubmitDisabled(false)
 	} else if(bad.includes(value)) {
 		document.getElementById('opinionSamy').classList.add('is-invalid')
 		document.getElementById('opinionSamyBad').style.display = 'block'
+		setSubmitDisabled(true)
 	} else {
 		document.getElementById('opinionSamy').classList.remove('is-valid')
 		document.getElementById('opinionSamy').classList.remove('is-invalid')
 		document.getElementById('opinionSamyGood').style.display = 'none'
 		document.getElementById('opinionSamyBad').style.display = 'none'
+		setSubmitDisabled(false)
 	}
 }
 
@@ -21,14 +28,17 @@ const onWebsiteOpinionChange = value => {
 	if(good.includes(value)) {
 		document.getElementById('opinionWebsite').classList.add('is-valid')
 		document.getElementById('opinionWebsiteGood').style.display = 'block'
+		setSubmitDisabled(false)
 	} else if(bad.includes(value)) {
 		document.getElementById('opinionWebsite').classList.add('is-invalid')
 		document.getElementById('opinionWebsiteBad').style.display = 'block'
+		setSubmitDisabled(true)
 	} else {
 		document.getElementById('opinionWebsite').classList.remove('is-valid')
 		document.getElementById('opinionWebsite').classList.remove('is-invalid')
 		document.getElementById('opinionWebsiteGood').style.display = 'none'
 		document.getElementById('opinionWebsiteBad').style.display = 'none'
+		setSubmitDisabled(false)
 	}
 }
 
@@ -36,13 +46,16 @@ const onOwnerOpinionChange = value => {
 	if(good.includes(value)) {
 		document.getElementById('opinionOwner').classList.add('is-valid')
 		document.getElementById('opinionOwnerGood').style.display = 'block'
+		setSubmitDisabled(false)
 	} else if(bad.includes(value)) {
 		document.getElementById('opinionOwner').classList.add('is-invalid')
 		document.getElementById('opinionOwnerBad').style.display = 'block'
+		setSubmitDisabled(true)
 	} else {
 		document.getElementById('opinionOwner').classList.remove('is-valid')
 		document.getElementById('opinionOwner').classList.remove('is-invalid')
 		document.getElementById('opinionOwnerGood').style.display = 'none'
 		document.getElementById('opinionOwnerBad').style.display = 'none'
+		setSubmitDisabled(false)
 	}
 }
