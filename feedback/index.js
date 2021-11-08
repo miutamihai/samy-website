70c72e73de1dfe07dd16786792ad4281faaf3f4d
miutamihai <miuta.mihai@gmail.com>
2021-11-08 16:35:51 +0200

    [master] Add website opinion

--- feedback/index.js
+++ feedback/index.js
@@ -1,6 +1,6 @@
-const good = ['nice', 'pretty', 'beautiful', 'cool', 'Nice', 'Pretty', 'Beautiful', 'Cool']
+const good = ['nice', 'pretty', 'beautiful', 'cool', 'good', 'Good', 'Nice', 'Pretty', 'Beautiful', 'Cool']
 
-const bad = ['ugly', 'nasty', 'lame', 'Ugly', 'Nasty', 'Lame']
+const bad = ['ugly', 'nasty', 'lame', 'bad', 'Bad', 'Ugly', 'Nasty', 'Lame']
 
 const onSamyOpinionChange = value => {
 	if(good.includes(value)) {
@@ -14,5 +14,20 @@ const onSamyOpinionChange = value => {
 		document.getElementById('opinionSamy').classList.remove('is-invalid')
 		document.getElementById('opinionSamyGood').style.display = 'none'
 		document.getElementById('opinionSamyBad').style.display = 'none'
+	}
+}
+
+const onWebsiteOpinionChange = value => {
+	if(good.includes(value)) {
+		document.getElementById('opinionWebsite').classList.add('is-valid')
+		document.getElementById('opinionWebsiteGood').style.display = 'block'
+	} else if(bad.includes(value)) {
+		document.getElementById('opinionWebsite').classList.add('is-invalid')
+		document.getElementById('opinionWebsiteBad').style.display = 'block'
+	} else {
+		document.getElementById('opinionWebsite').classList.remove('is-valid')
+		document.getElementById('opinionWebsite').classList.remove('is-invalid')
+		document.getElementById('opinionWebsiteGood').style.display = 'none'
+		document.getElementById('opinionWebsiteBad').style.display = 'none'
 	}
 }
